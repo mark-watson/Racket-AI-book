e8b5a8245d58b043d21d43fee186779aafad2861
Mark Watson <markw@markwatson.com>
2024-06-13 14:31:17 -0700

    changed default Ollama model

--- source-code/llmapis/ollama_ai_local.rkt
+++ source-code/llmapis/ollama_ai_local.rkt
@@ -9,7 +9,7 @@
 (define (helper prompt . model-name)
   (displayln (list "Model name: " model-name))
   (let* ((model
-          (if (equal? model-name '()) "mistral" (first (first model-name))))
+          (if (equal? model-name '(())) "mistral:v0.3" (first (first model-name))))
          (prompt-data
           (string-join
            (list
@@ -23,7 +23,7 @@
            "http://localhost:11434/api/generate"
            #:data prompt-data))
          (r (response-json p)))
-    ;;(displayln r)
+    (displayln r)
     (hash-ref r 'response)))
 
 (define (question-ollama-ai-local question . model-name)
@@ -38,7 +38,7 @@
 
 
 ;;(displayln (question-ollama-ai-local "Mary is 30 and Harry is 25. Who is older and by how much?"))
-;;(displayln (question-ollama-ai-local "Mary is 30 and Harry is 25. Who is older and by how much?" "mixtral:8x7b-instruct-v0.1-q2_K"))
+;;(displayln (question-ollama-ai-local "Mary is 30 and Harry is 25. Who is older and by how much?" "llama3:latest"))
 ;;(displayln (completion-ollama-ai-local "Frank bought a new sports car. Frank drove"))
 ;; The default mistral-7b model almost always gets the following wrong:
 ;;(displayln (question-ollama-ai-local "Mary is 30, Bob is 25, and Susan is 32. Describe all combinations of the three people comparing their ages, including the calculation of age differences. Be concise."))
@@ -72,15 +72,3 @@
 
 ;; (embeddings-ollama "Here is an article about llamas...")
 
-#|
-curl http://localhost:11434/api/embeddings -d '{
-  "model": "mistral:7b-instruct",
-  "prompt": "Here is an article about llamas..."
-}'
-
-curl http://localhost:11434/api/embeddings -d '{
-  "model": "mistral",
-  "prompt": "Here is an article about llamas..."
-}'
-
-|#
